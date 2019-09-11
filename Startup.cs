4e2a6ab6b85085bcf19630adb9123139bb9fd6a9
jcarmartin <jcam@jcamweb.com>
2019-09-11 06:38:49 +0200

    Ver. 2.1

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace BasicCalculatorCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             // Register the Swagger generator
             services.AddSwaggerGen(c =>
             {
